c52b2ff5cd553cf9889ecfea435196dec53b6d20
Raj Kumar <raj.vishwakarma@encoresky.com>
2022-08-18 16:21:15 +0530

    add example json

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
 import { useRouter } from "next/router";
-
+import examples from "../examples.json";
 export default function Home() {
   const router = useRouter();
   return (
     <Box sx={{ p: 4 }}>
       <Typography variant="h6">Explore Examples</Typography>
-      <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
-        <Button onClick={() => router.push("/ag-grid")}>AG-Grid</Button>
+      <Stack direction="column" spacing={2} sx={{ mt: 2, width: 100 }}>
+        {examples.map((e) => (
+          <Button onClick={() => router.push(e.route)}>{e.title}</Button>
+        ))}
       </Stack>
     </Box>
   );
